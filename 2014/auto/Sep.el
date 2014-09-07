f2062c55651d894f1af2fa96f3aaabfb81839fe8
Abraham Xiao <q289196573@gmail.com>
2014-09-08 01:17:48 +0400

    Progress updated. Some more listening, then go to bed. Early to rise since you have a lot to read & write, and MAKE UP.

---
+++ 2014/auto/Sep.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook "Sep"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec-1"
+     "sec-1-1"
+     "sec-1-2"
+     "sec-1-2-1"
+     "sec-1-2-2")
+    (TeX-run-style-hooks
+     "hyperref"
+     "amssymb"
+     "wasysym"
+     "marvosym"
+     "textcomp"
+     "amsmath"
+     "ulem"
+     "normalem"
+     "rotating"
+     "wrapfig"
+     "float"
+     "longtable"
+     "graphicx"
+     "fixltx2e"
+     ""
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "art11"
+     "article"
+     "11pt")))
+
